4af95ddd53857f421122ecf86da692c785d29851
sigu <sigu@podiihq.com>
2020-10-20 15:58:29 +0300

    create sbom file

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule SbomPoc.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:sbom, "~> 0.6.0", only: :dev, runtime: false}
     ]
   end
 
