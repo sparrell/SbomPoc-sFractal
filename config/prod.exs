fdfc2ae782fac637a2fd2124b1ab2b84d2f08fd1
Duncan Sparrell <duncan@sfractal.com>
2020-10-24 11:53:33 -0400

    debug in production

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,9 @@ config :sbom_poc, SbomPocWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+#config :logger, level: :info
+config :logger, level: :debug
+
 
 config :sbom_poc, SbomPocWeb.Endpoint, server: true
 
