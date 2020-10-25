9f424c54ca96a2ee921b78a46f3347f33e49183a
Duncan Sparrell <duncan@sfractal.com>
2020-10-24 22:58:20 -0400

    home page

--- lib/sbomPoc_web/live/home_live.ex
+++ lib/sbomPoc_web/live/home_live.ex
@@ -13,8 +13,33 @@ defmodule SbomPocWeb.HomeLive do
   @impl true
   def render(assigns) do
     ~L"""
-    <h1>Home page view </h1>
-    stuff
+    action item - replace phoenic logo
+    with plugfest logo
+    <h1>Home page </h1>
+    Info on sfractal machines:
+    <bl>
+      <li>sbompoc.sfractal.com</li>
+      <li>twinklymaha.sfractal.com</li>
+      <li>twinklyhaha.sfractal.com</li>
+      <li>blinkymaha.sfractal.com</li>
+      <li>blinkyhaha.sfractal.com</li>
+      <li>vulnerable.twinklymaha.sfractal.com</li>
+      <li> </li>
+    </bl>
+    <h2>sbompoc.sfractal.com</h2>
+    <p>This is running on GCP vm buddhabrot</p>
+    <p>/ is this home page</p>
+    <p>/events is mqtt log </p>
+    <p>/phoenix is phoenix info page </p>
+    <p>/dashboard is beam under-the-hood info for debugging</p>
+    <p> </p>
+    <p>more stuff</p>
+    <h2>x.sfractal.com</h2>
+    <p> </p>
+    <h2>x.sfractal.com</h2>
+    <p> </p>
+    <h2>x.sfractal.com</h2>
+    <p> </p>
     """
   end
 
