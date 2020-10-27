109311adc174034084763d11d77a248ee92d9c0a
Duncan Sparrell <duncan@sfractal.com>
2020-10-27 10:54:32 -0400

    update homepage

--- lib/sbom_poc_web/live/home_live.ex
+++ lib/sbom_poc_web/live/home_live.ex
@@ -13,12 +13,11 @@ defmodule SbomPocWeb.HomeLive do
   @impl true
   def render(assigns) do
     ~L"""
-    action item - replace phoenic logo
-    with plugfest logo
     <h1>Home page </h1>
     Info on sfractal machines:
     <bl>
       <li>sbompoc.sfractal.com</li>
+      <li>analysis.sbompoc.sfractal.com</li>
       <li>twinklymaha.sfractal.com</li>
       <li>twinklyhaha.sfractal.com</li>
       <li>blinkymaha.sfractal.com</li>
@@ -27,17 +26,39 @@ defmodule SbomPocWeb.HomeLive do
     </bl>
     <h2>sbompoc.sfractal.com</h2>
     <p>This is running on GCP vm buddhabrot</p>
-    <p>home page http://sbompoc.sfractal.com:4000/</p>
-    <p>events is mqtt log http://sbompoc.sfractal.com:4000/events</p>
-    <p>??? is sbom and dependency tracker add-link-here</p>
-    <p>phoenix is phoenix info page http://sbompoc.sfractal.com:4000/phoenix</p>
-    <p>dashboard is beam under-the-hood info for debugging http://sbompoc.sfractal.com:4000/dashboard/sbom_poc%40buddhabrot/home</p>
-    <p> </p>
+    <p>home page
+    <a href="http://sbompoc.sfractal.com:4000/">
+    http://sbompoc.sfractal.com:4000/</a>
+    </p>
+    <p>events is mqtt log
+    <a href="http://sbompoc.sfractal.com:4000/events">
+    http://sbompoc.sfractal.com:4000/events</a>
+    </p>
+    <p>phoenix is phoenix info page
+    <a href="http://sbompoc.sfractal.com:4000/phoenix">
+    http://sbompoc.sfractal.com:4000/phoenix</a>
+    </p>
+    <p>dashboard is beam under-the-hood info for debugging
+    <a href="http://sbompoc.sfractal.com:4000/dashboard/sbom_poc%40buddhabrot/home">
+    http://sbompoc.sfractal.com:4000/dashboard/sbom_poc%40buddhabrot/home</a>
+    </p>
+
+    <h2>analysis.sbompoc.sfractal.com</h2>
+    <p>sbom dependency/vulnerability http://analysis.sbompoc.sfractal.com:8080/</p>
+    <p>has <a href="http://analysis.sbompoc.sfractal.com:8080/projects/4bf2c440-f4bf-4d4f-b02c-fea518b5d6a2">SbomPoc</a>
+    and <a href="http://analysis.sbompoc.sfractal.com:8080/projects/f66c3983-35ab-4741-81b5-acab2a936ced">twinklymaha</a>
+    SBOMs loaded.</p>
+    <p>see component lists(one-hop plus, but not complete)
+    by clicking on dependencies within project
+    </p>
 
     <h2>twinklymaha.sfractal.com</h2>
     <p>Twinkly (ie digital twin of blinky ie web leds)
     MaHa (ie Mqtt Api Helloworld Actuator)
-    http://twinklymaha.sbompoc.sfractal.com:4000/</p>
+    <a href="http://twinklymaha.sbompoc.sfractal.com:4000/">
+    http://twinklymaha.sbompoc.sfractal.com:4000/</a>
+    </p>
+
     <h2>twinklyhaha.sfractal.com</h2>
     <p>Twinkly (ie digital twin of blinky ie web leds)
     HaHa (ie Http Api Helloworld Actuator)</p>
